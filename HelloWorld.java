9bc51dabfdc68e517f68f88521b9991e9d76cfe1
skodumuru <kalakodumuri@gmail.com>
2016-11-25 16:13:29 +0530

    modified java file

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,8 @@
 public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
-      System.out.println("Hello, World");
+      int i;
+      for (i=0;i<10;i++)
+        System.out.println("Saying Hello, World for "+i+" time");
    }
 }
