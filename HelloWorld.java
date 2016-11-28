027d432a9a853568bd381c7aee450f861234284c
skodumuru <kalakodumuri@gmail.com>
2016-11-28 15:52:07 +0530

    modified java file

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,14 @@
+package example;
+
 public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
       int i;
       for (i=0;i<10;i++)
         System.out.println("Saying Hello, World for "+i+" time");
+   }
+   HelloWorld()
+   {
+     System.out.println("added default constructor");
    }
 }
