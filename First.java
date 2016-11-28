87316723f3afd06d3afc34b491f7b639c69494c4
skodumuru <kalakodumuri@gmail.com>
2016-11-28 15:49:45 +0530

    added java file

---
+++ First.java
@@ -0,0 +1,14 @@
+package example;
+
+class First
+{
+
+   First()
+   {
+     System.out.println("default constructor");
+   }
+   public static void main(String[] args) {
+        System.out.println("This is my first program ");
+   }
+    
+}
